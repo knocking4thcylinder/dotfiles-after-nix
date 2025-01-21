63d3d9b43bed1d9af126a22f1dd32a3cd4efaf9f
knocking4thcylinder <lev.fedorets@ya.ru>
2025-01-22 00:12:28 +0300

    .

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,7 +20,7 @@ return {
 			},
 
 			groups = {
-				border = "muted",
+				border = "rounded",
 				link = "iris",
 				panel = "surface",
 
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,10 +1,16 @@
 return {
 	"echasnovski/mini.nvim",
-    version = false,
-    config = function()
-        require("mini.statusline").setup()
-        require("mini.pairs").setup()
-        require("mini.git").setup()
-        require("mini.files").setup()
-    end,
+	version = false,
+	config = function()
+		require("mini.statusline").setup()
+		require("mini.pairs").setup()
+		require("mini.git").setup()
+		require("mini.files").setup({ windows = { border = "rounded" } })
+		vim.keymap.set("n", "<C-n>", "<CMD>lua MiniFiles.open()<CR>")
+		require("mini.notify").setup()
+		require("mini.surround").setup()
+		require("mini.operators").setup()
+		require("mini.comment").setup()
+		require("mini.starter").setup()
+	end,
 }
