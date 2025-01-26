b27297b2e7392049045c28408e805ff660f125ee
knocking4thcylinder <lev.fedorets@ya.ru>
2025-01-27 01:06:04 +0300

    some config updates

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,62 +1,76 @@
 return {
-	"echasnovski/mini.nvim",
-	version = false,
-	config = function()
-		require("mini.statusline").setup()
-		require("mini.pairs").setup()
-		require("mini.splitjoin").setup()
-		require("mini.clue").setup()
-		require("mini.git").setup()
-		require("mini.files").setup({ windows = { border = "rounded" } })
-		vim.keymap.set("n", "<C-n>", "<CMD>lua MiniFiles.open()<CR>")
-		-- require("mini.notify").setup()
-		require("mini.surround").setup()
-		require("mini.operators").setup()
-		require("mini.comment").setup()
-		require("mini.starter").setup()
-		local miniclue = require("mini.clue")
-		miniclue.setup({
-			triggers = {
-				-- Leader triggers
-				{ mode = "n", keys = "<Leader>" },
-				{ mode = "x", keys = "<Leader>" },
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+        require("mini.statusline").setup()
+        require("mini.icons").setup()
+        local hipatterns = require("mini.hipatterns")
+        hipatterns.setup({
+            highlighters = {
+                -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+                fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+                hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+                todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+                note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-				-- Built-in completion
-				{ mode = "i", keys = "<C-x>" },
+                -- Highlight hex color strings (`#rrggbb`) using that color
+                hex_color = hipatterns.gen_highlighter.hex_color(),
+            },
+        })
+        require("mini.pairs").setup()
+        require("mini.splitjoin").setup()
+        require("mini.clue").setup()
+        require("mini.git").setup()
+        require("mini.files").setup({ windows = { border = "rounded" } })
+        vim.keymap.set("n", "<C-n>", "<CMD>lua MiniFiles.open()<CR>")
+        -- require("mini.notify").setup()
+        require("mini.surround").setup()
+        require("mini.operators").setup()
+        require("mini.comment").setup()
+        require("mini.starter").setup()
+        local miniclue = require("mini.clue")
+        miniclue.setup({
+            triggers = {
+                -- Leader triggers
+                { mode = "n", keys = "<Leader>" },
+                { mode = "x", keys = "<Leader>" },
 
-				-- `g` key
-				{ mode = "n", keys = "g" },
-				{ mode = "x", keys = "g" },
+                -- Built-in completion
+                { mode = "i", keys = "<C-x>" },
 
-				-- Marks
-				{ mode = "n", keys = "'" },
-				{ mode = "n", keys = "`" },
-				{ mode = "x", keys = "'" },
-				{ mode = "x", keys = "`" },
+                -- `g` key
+                { mode = "n", keys = "g" },
+                { mode = "x", keys = "g" },
 
-				-- Registers
-				{ mode = "n", keys = '"' },
-				{ mode = "x", keys = '"' },
-				{ mode = "i", keys = "<C-r>" },
-				{ mode = "c", keys = "<C-r>" },
+                -- Marks
+                { mode = "n", keys = "'" },
+                { mode = "n", keys = "`" },
+                { mode = "x", keys = "'" },
+                { mode = "x", keys = "`" },
 
-				-- Window commands
-				{ mode = "n", keys = "<C-w>" },
+                -- Registers
+                { mode = "n", keys = '"' },
+                { mode = "x", keys = '"' },
+                { mode = "i", keys = "<C-r>" },
+                { mode = "c", keys = "<C-r>" },
 
-				-- `z` key
-				{ mode = "n", keys = "z" },
-				{ mode = "x", keys = "z" },
-			},
+                -- Window commands
+                { mode = "n", keys = "<C-w>" },
 
-			clues = {
-				-- Enhance this by adding descriptions for <Leader> mapping groups
-				miniclue.gen_clues.builtin_completion(),
-				miniclue.gen_clues.g(),
-				miniclue.gen_clues.marks(),
-				miniclue.gen_clues.registers(),
-				miniclue.gen_clues.windows(),
-				miniclue.gen_clues.z(),
-			},
-		})
-	end,
+                -- `z` key
+                { mode = "n", keys = "z" },
+                { mode = "x", keys = "z" },
+            },
+
+            clues = {
+                -- Enhance this by adding descriptions for <Leader> mapping groups
+                miniclue.gen_clues.builtin_completion(),
+                miniclue.gen_clues.g(),
+                miniclue.gen_clues.marks(),
+                miniclue.gen_clues.registers(),
+                miniclue.gen_clues.windows(),
+                miniclue.gen_clues.z(),
+            },
+        })
+    end,
 }
