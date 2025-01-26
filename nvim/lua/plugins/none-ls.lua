63e2002b4fc48787421ab416dec52d56e6c428a9
knocking4thcylinder <lev.fedorets@ya.ru>
2025-01-27 00:48:30 +0300

    some nvim chenanigans + gitignore stuff

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,6 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.diagnostics.mypy,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.black,
