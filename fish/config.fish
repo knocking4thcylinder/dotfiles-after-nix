b970b69004caa7c8d6c7c47ed033b4a556de2416
knocking4thcylinder <lev.fedorets@ya.ru>
2025-03-25 22:49:43 +0300

    added better kotlin and fish support to neovim

--- fish/config.fish
+++ fish/config.fish
@@ -2,27 +2,31 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux LS_COLORS (vivid generate rose-pine)
+set -gx LS_COLORS (vivid generate rose-pine)
 
-set -Ux FZF_DEFAULT_OPTS "
+set -gx FZF_DEFAULT_OPTS "
 	--color=fg:#908caa,bg:#101010,hl:#ebbcba
 	--color=fg+:#e0def4,bg+:#101010,hl+:#ebbcba
 	--color=border:#403d52,header:#31748f,gutter:#191724
 	--color=spinner:#f6c177,info:#9ccfd8
 	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
 
-set -Ux TRIK_PYTHONPATH (python3.11 -c 'import sys; import os; print(os.pathsep.join(sys.path))')
+set -gx TRIK_PYTHONPATH (python3.11 -c 'import sys; import os; print(os.pathsep.join(sys.path))')
 
-alias lg="lazygit"
-alias vi="nvim"
-alias vim="nvim"
-alias ls="eza --icons=always --color=always"
-alias ll="eza -l --icons=always --color=always"
-alias l="eza -la --icons=always --color=always"
+set -gx GRADLE_PATH "$HOME/thirdparty/gradle-8.11.1/bin/"
+set -gx KOTLIN_PATH "$HOME/thirdparty/kotlinc/bin/"
+set -gx KOTLIN_NATIVE_PATH "$HOME/thirdparty/kotlin-native-prebuilt-linux-x86_64-2.1.20/bin/"
+
+set -gx PATH "$PATH:$KOTLIN_NATIVE_PATH:$KOTLIN_PATH:$GRADLE_PATH"
+abbr -a lg lazygit
+abbr -a vi nvim
+abbr -a vim nvim
+abbr -a ls "eza --icons=always --color=always"
+abbr -a ll "eza -l --icons=always --color=always"
+abbr -a l "eza -la --icons=always --color=always"
 
 starship init fish | source
 atuin init --disable-up-arrow fish | source
-
 
 # >>> coursier install directory >>>
 set -gx PATH "$PATH:/home/lev-arch/.local/share/coursier/bin:/app"
@@ -31,6 +35,6 @@ set -gx PATH "$PATH:/home/lev-arch/.local/share/coursier/bin:/app"
 # pnpm
 set -gx PNPM_HOME "/home/lev-arch/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,9 +7,10 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.diagnostics.ktlint,
+                -- null_ls.builtins.diagnostics.ktlint,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.ktlint,
+                -- null_ls.builtins.formatting.ktfmt,
                 null_ls.builtins.formatting.mix,
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.isort,
