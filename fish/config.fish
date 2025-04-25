a63dd64c0b1eed09f9564c8213a43d18c1045ddc
knocking4thcylinder <lev.fedorets@ya.ru>
2025-04-25 11:43:51 +0300

    moved from starship to tide

--- fish/config.fish
+++ fish/config.fish
@@ -4,17 +4,9 @@ end
 
 set -gx LS_COLORS (vivid generate rose-pine | string replace -a "49;116;143" "156;207;216")
 
-
-set -gx FZF_DEFAULT_OPTS "
-	--color=fg:#908caa,bg:#101010,hl:#ebbcba
-	--color=fg+:#e0def4,bg+:#101010,hl+:#ebbcba
-	--color=border:#403d52,header:#9ccfd8,gutter:#191724
-	--color=spinner:#f6c177,info:#9ccfd8
-	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
-
 set -gx TRIK_PYTHONPATH (python3.11 -c 'import sys; import os; print(os.pathsep.join(sys.path))')
 
-set -gx EDITOR "nvim"
+set -gx EDITOR nvim
 set -gx GRADLE_PATH "$HOME/thirdparty/gradle-8.11.1/bin/"
 set -gx KOTLIN_PATH "$HOME/thirdparty/kotlinc/bin/"
 set -gx KOTLIN_NATIVE_PATH "$HOME/thirdparty/kotlin-native-prebuilt-linux-x86_64-2.1.20/bin/"
@@ -26,11 +18,20 @@ set -gx PATH "$PATH:$MIXPATH:$GOPATH:$GOPATH2:$KOTLIN_NATIVE_PATH:$KOTLIN_PATH:$
 abbr -a lg lazygit
 abbr -a vi nvim
 abbr -a vim nvim
-abbr -a ls "eza --icons=always --color=always"
-abbr -a ll "eza -l --icons=always --color=always"
-abbr -a l "eza -la --icons=always --color=always"
 
-starship init fish | source
+function ls --description 'List contents of directory'
+    eza --icons=always --color=always $argv
+end
+
+function ll --wraps=ls --description 'List contents of directory using long format'
+    eza -l --icons=always --color=always $argv
+end
+
+function la --wraps=ls --description 'List contents of directory, including hidden files in directory using long format'
+    eza -lA --icons=always --color=always $argv
+end
+
+# starship init fish | source
 carapace _carapace fish | source
 atuin init --disable-up-arrow fish | source
 
